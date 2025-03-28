3fec91a5aece5dcead4e4a263cc97f52f9f97c1e
julian-newera <julian@newera.finance>
2025-03-28 22:59:55 +0900

    updated contract & tests

--- contracts/script/PriceAvsHook.s.sol
+++ contracts/script/PriceAvsHook.s.sol
@@ -17,7 +17,7 @@ $$    $$/   $$  $$/ $$ |  $$ |$$       |$$ |  $$ |  $$  $$/ $$ |$$       |
  */
 import {Script, console} from "forge-std/Script.sol";
 import {IAttestationCenter} from "../src/interfaces/IAttestationCenter.sol";
-import {DynamicPricesAvsHook} from "../src/PriceAvsHook.sol";
+import {NewEraHook} from "../src/NewEraHook.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {HookMiner} from "v4-periphery/src/utils/HookMiner.sol";
@@ -25,8 +25,8 @@ import {console} from "forge-std/console.sol";
 
 // How to:
 // Either `source ../../.env` or replace variables in command.
-// forge script script/PriceAvsHook.s.sol --via-ir --rpc-url https://base-sepolia.infura.io/v3/86d6db8b43814247b322f75d29ba345e --private-key 151ee9c063332f97069f4f2833c32878a3e35a77070869fae3c0c6050c055528 --broadcast -vvvv --verify --etherscan-api-key 86d6db8b43814247b322f75d29ba345e --chain 84532 --verifier-url https://base-sepolia.etherscan.io/ --sig="run(address,address,uint256)" 0x822BFc76e35C8bCcCeb5e10aC429F7EcE10D3416 0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408 1000
-contract DynamicPricesAvsHookDeploy is Script {
+// forge script script/NewEraHook.s.sol --via-ir --rpc-url https://base-sepolia.infura.io/v3/86d6db8b43814247b322f75d29ba345e --private-key 151ee9c063332f97069f4f2833c32878a3e35a77070869fae3c0c6050c055528 --broadcast -vvvv --verify --etherscan-api-key 86d6db8b43814247b322f75d29ba345e --chain 84532 --verifier-url https://base-sepolia.etherscan.io/ --sig="run(address,address,uint256)" 0x822BFc76e35C8bCcCeb5e10aC429F7EcE10D3416 0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408 1000
+contract NewEraHookDeploy is Script {
     function setUp() public {}
 
     function run(address attestationCenter, address poolManager,  uint256 _expirationInterval) public {
@@ -43,13 +43,13 @@ contract DynamicPricesAvsHookDeploy is Script {
         bytes memory constructorArgs = abi.encode(address(attestationCenter), IPoolManager(address(poolManager)), _expirationInterval);
 
         (address hookAddress, bytes32 salt) =
-            HookMiner.find(CREATE2_DEPLOYER, flags, type(DynamicPricesAvsHook).creationCode, constructorArgs);
+            HookMiner.find(CREATE2_DEPLOYER, flags, type(NewEraHook).creationCode, constructorArgs);
 
         console.log("Mined hook address:", hookAddress);
         console.log("Salt:", vm.toString(salt));
 
         vm.startBroadcast();
-        DynamicPricesAvsHook avsHook = new DynamicPricesAvsHook{salt: salt}(address(attestationCenter), IPoolManager(address(poolManager)), _expirationInterval);
+        NewEraHook avsHook = new NewEraHook{salt: salt}(address(attestationCenter), IPoolManager(address(poolManager)), _expirationInterval);
 
         require(address(avsHook) == hookAddress, "Hook address mismatch");
 
@@ -57,4 +57,4 @@ contract DynamicPricesAvsHookDeploy is Script {
         vm.stopBroadcast();
     }
 }
-// forge script script/PriceAvsHook.s.sol --via-ir --rpc-url https://base-sepolia.infura.io/v3/86d6db8b43814247b322f75d29ba345e --private-key 151ee9c063332f97069f4f2833c32878a3e35a77070869fae3c0c6050c055528 --broadcast -vvvv --verify --etherscan-api-key 86d6db8b43814247b322f75d29ba345e --chain 84532 --verifier-url https://base-sepolia.etherscan.io/ --optimize --optimizer-runs 20 --sig="run(address,address,uint256)" 0x822BFc76e35C8bCcCeb5e10aC429F7EcE10D3416 0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408 1000
+// forge script script/NewEraHook.s.sol --via-ir --rpc-url https://base-sepolia.infura.io/v3/86d6db8b43814247b322f75d29ba345e --private-key 151ee9c063332f97069f4f2833c32878a3e35a77070869fae3c0c6050c055528 --broadcast -vvvv --verify --etherscan-api-key 86d6db8b43814247b322f75d29ba345e --chain 84532 --verifier-url https://base-sepolia.etherscan.io/ --optimize --optimizer-runs 20 --sig="run(address,address,uint256)" 0x822BFc76e35C8bCcCeb5e10aC429F7EcE10D3416 0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408 1000
